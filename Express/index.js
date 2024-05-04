72041bb3972e8978bf37e77a5ee40ae7e8ef6e42
Anshuman <anshumanshukla.12as@gmail.com>
2024-05-04 08:10:57 +0530

    Backend

---
+++ Express/index.js
@@ -0,0 +1,23 @@
+// Creating an http-server
+// express
+// node default library => no
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+function sum(n){
+    let ans = 0;
+    for(let i=0;i<=n;i++){
+        ans = ans + i;
+    }
+    return ans;
+}
+app.get('/',function(req,res){
+    const n = req.query.n;
+    const ans = sum(n);
+    res.send("Hey Your anwer is "+ans);
+})
+app.listen(port,function(){
+    console.log(`Working port is ${port}`);
+})
