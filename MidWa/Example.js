4a93a993a20898e71afc6b3825761db76c0154a0
Anshuman <anshumanshukla.12as@gmail.com>
2024-05-05 18:46:48 +0530

    MiddleWare

---
+++ MidWa/Example.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+let numberOfRequest =0;
+
+function calculateRequests(req,res,next){
+    numberOfRequest++;
+    console.log(numberOfRequest);
+    next();
+}
+
+app.use(express.json());
+
+// We can pass function in app.use(functionName)
+
+app.get('/health-checkup',function(req,res){
+    console.log(req.body);
+    res.json({
+        msg: "Hey Anshuman"
+    })
+})
+
+app.listen(port);
