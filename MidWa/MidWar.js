87058d71035aea7b02ebea24b57f62fd26e6a1d4
Anshuman <anshumanshukla.12as@gmail.com>
2024-05-05 09:19:50 +0530

    MiddleWare

--- MidWa/MidWar.js
+++ MidWa/MidWar.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/health-checkup',function(req,res){
+app.get('/health-checkup',function(req,res,next){  //Multiple route handler can accept function as argument
     console.log("Hey from req 1");
 }),function(req,res){
     console.log("Hey From req 2");
