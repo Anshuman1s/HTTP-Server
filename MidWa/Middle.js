886989fc73994e3da085e1dd89ebbc12aed7a030
Anshuman <anshumanshukla.12as@gmail.com>
2024-05-18 00:03:33 +0530

    MiddleWare

--- MidWa/Middle.js
+++ MidWa/Middle.js
@@ -12,7 +12,7 @@ app.get('/health-checkup',function(req,res){
     res.send("You want to check  your Health-checkup . ");
 })
 app.get('/health-checkup2',function(req,res){
-    res.send("You want to check your Heart-checkup .")
+    res.send("You want to check your Heart-checkup . ")
 })
 app.get('/health-checkup',function(req,res){
 
