1874a52e3bf7fe6982cd75ca526d7c06870f6dc4
Anshuman <anshumanshukla.12as@gmail.com>
2024-05-18 00:04:51 +0530

    MiddleWare

--- MidWa/index.js
+++ MidWa/index.js
@@ -8,7 +8,7 @@ app.get('/healthCheckup',function(req,res){
     const kidneyId = req.query.kidneyId;
 
     if(username != "Anshuman" || password !='password123'){
-        res.status(400).json({"msg": "Somthing up with you inputs"});
+        res.status(400).json({"msg": "Somthing up with you inputs."});
         // do something with kideney
         return 
         
