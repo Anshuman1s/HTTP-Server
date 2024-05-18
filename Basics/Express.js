62a594f9b3f9c30f1915ec0ebf101894efe3752f
Anshuman <anshumanshukla.12as@gmail.com>
2024-05-19 02:22:21 +0530

    Express

--- Basics/Express.js
+++ Basics/Express.js
@@ -6,6 +6,9 @@ const PORT = 2100;
 app.get('/',function(req,res){
     res.send("Anshuman Shukla.")
 })
+app.put('/',function(req,res){
+    
+})
 
 app.listen(PORT,function(){
     console.log(`Server is running on  ${PORT}`);
