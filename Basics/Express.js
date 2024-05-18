eaa1f7f0fdcc3685f6653190bc340a3ddf7b68b1
Anshuman <anshumanshukla.12as@gmail.com>
2024-05-19 02:23:08 +0530

    Express

--- Basics/Express.js
+++ Basics/Express.js
@@ -7,7 +7,7 @@ app.get('/',function(req,res){
     res.send("Anshuman Shukla.")
 })
 app.put('/',function(req,res){
-    res.send("Put method")
+    res.send("Put method.")
 })
 
 app.listen(PORT,function(){
