cd7841cef54625c73074b99e77e85418fd81784e
Anshuman <anshumanshukla.12as@gmail.com>
2024-05-22 20:22:46 +0530

    Basics

--- Basics/HTTP-Server.js
+++ Basics/HTTP-Server.js
@@ -11,7 +11,7 @@ app.get('/',function(req,res){
 })
 
 app.post('/',function(req,res){
-    res.send("Message Recieved")
+    res.send("Message Recieved.")
 })
 app.listen(PORT,function(){
     console.log(`Port Listeing on ${PORT}`);
