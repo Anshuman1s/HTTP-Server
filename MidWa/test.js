51c781b9f9ec0c8a8be9cc6dcb026332065ef8bd
Anshuman <anshumanshukla.12as@gmail.com>
2024-05-28 07:14:45 +0530

    test

---
+++ MidWa/test.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+console.log(express);
+console.log(app);
+
+// app.get('/login',function(req,res){
+//     res.send('Hey')
+// });
+
+// app.listen(port,function(){
+//     console.log(`Server is runnng on port number: ${port}`);
+// })
+
+app.post('/upload',function(req,res){
+    
+})
