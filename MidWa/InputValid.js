10be4f71d1973a4c2cac597c6d0c42ee71c7b58f
Anshuman shukla <117532540+Anshuman1s@users.noreply.github.com>
2024-07-18 17:05:00 +0530

    Update InputValid.js

--- MidWa/InputValid.js
+++ MidWa/InputValid.js
@@ -25,4 +25,7 @@ app.use(function(err,req,res,next){
         msg:"Sorry Something is up with our Server"
     })
 })
+app.get('/',function(req,res){
+    res.send("Anshuman");
+}
 app.listen(port);
